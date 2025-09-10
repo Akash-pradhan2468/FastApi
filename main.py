7ec47e39fd055051073113c23cc29e3510dc995a
Akash-pradhan2468 <akash2468pradhan@gmail.com>
2025-09-10 22:16:32 +0530

    path parametr and query parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app=FastAPI()
 
@@ -23,3 +23,30 @@ def about():
 def view():
     data=loadData()
     return data
+
+#Path parameter- these are the part of the url but these are variable ex user-id and mandatory /patient/{patient_id}
+#Query parameter-http://127.0.0.1:8000/sort?sort_by=height&order_by=desc not mando tary
+
+@app.get('/patient/{patient_id}')
+# def get_patient(patient_id:str): can do this also
+def get_patient(patient_id:str=Path(..., description="id of the patient",example="p001")):
+    data=loadData()
+    print(patient_id)
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404,detail="Patient not found")
+@app.get('/sort')
+def sort_patients(sort_by:str=Query(..., description="Sort on the basis of height weight and bmi"),order_by:str=Query('asc',description="desc or ascending")):
+    valid_fields=['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail="please choose a valid parameter to sort")
+    
+    if order_by not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail="PLsease provide asc or desc for sorting order")
+    
+
+    data=loadData()
+    sort_order=True if order_by == 'desc'else False
+    sorteddata=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorteddata
